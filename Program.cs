6ca4f96b2f948098dd7bb0ac11e9bac342e77678
Aleksandr P <pishite2011@yandex.ru>
2022-10-25 20:11:37 +0500

    TASK 66

--- Program.cs
+++ Program.cs
@@ -1,24 +1,45 @@
-﻿// Задача 64: Задаю значение N. Программа, выводит все натуральные числа в промежутке от N до 1. Выполнено с помощью рекурсии.
-int n = InputInt("Введите положительное число: ");
-int m = 1;
-if (n < 1)
+﻿void Task64()
 {
-    Console.WriteLine("Вы ввели не положительное число!");
-}
-else
-Console.WriteLine(NaturalNumber(n, m));
-
-int NaturalNumber(int n, int m)
-{
-    if (n == m)
-        return n;
+    // Задача 64: Задаю значение N. Программа, выводит все натуральные числа в промежутке от N до 1. Выполнено с помощью рекурсии.
+    int n = InputInt("Введите положительное число: ");
+    int m = 1;
+    if (n < 1)
+    {
+        Console.WriteLine("Вы ввели не положительное число!");
+    }
     else
-        Console.Write($"{NaturalNumber(n, m + 1)}, ");
-    return m;
+        Console.WriteLine(NaturalNumber(n, m));
+
+    int NaturalNumber(int n, int m)
+    {
+        if (n == m)
+            return n;
+        else
+            Console.Write($"{NaturalNumber(n, m + 1)}, ");
+        return m;
+    }
+
+    int InputInt(string output)
+    {
+        Console.Write(output);
+        return int.Parse(Console.ReadLine());
+    }
 }
+
+// Задача 66: Задю значения M и N. Программа, ищет сумму натуральных элементов в промежутке от M до N.
+int m = InputInt("Введите M: ");
+int n = InputInt("Введите N: ");
+Console.WriteLine($"Сумма элементов от {m} до {n} равна {CountNaturalSum(m, n)}");
 
 int InputInt(string output)
 {
     Console.Write(output);
     return int.Parse(Console.ReadLine());
+}
+
+int CountNaturalSum(int m, int n)
+{
+    if (m == n)
+        return n;
+    return n + CountNaturalSum(m, n - 1);
 }
